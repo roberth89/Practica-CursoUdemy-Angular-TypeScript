6e1a1f93a86258b2784a9044fcae200f49221245
Roberto Cordero Quirós <RC4691CP@grupomonge.com>
2021-09-04 13:31:43 -0600

    commit to master

---
+++ app.ts
@@ -0,0 +1,21 @@
+(function(){
+
+    function saludar(nombre:string) {
+        console.table('Hola ' + nombre); // Hola Logan
+    }
+    
+    
+    const wolverine = {
+        nombre: 'Logan'
+    };
+    
+    
+    saludar(wolverine.nombre);
+    
+    // Recordatorio abrir como los arhcivos ts, el navegador
+    // no lo reconoce se debe entrar a la terminal 
+    // para convertir el archivo ts a js
+
+})();
+
+
