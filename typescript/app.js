06704aa993afaad3809f7aeed9362ce996ce9d18
Roberto Cordero Quirós <RC4691CP@grupomonge.com>
2021-09-04 23:05:32 -0600

    interface en TypeScrpt

--- typescript/app.js
+++ typescript/app.js
@@ -1,4 +1,15 @@
 "use strict";
-// Practica promesas retirar dinero
 (() => {
+    const enviarMision = (xmen) => {
+        console.log(`Enviando a ${xmen.nombre} a la mision`);
+    };
+    const regresarMision = (xmen) => {
+        console.log(`Regresando a ${xmen.nombre} a la mision`);
+    };
+    const wolverine = {
+        nombre: 'Logan',
+        edad: 60
+    };
+    enviarMision(wolverine);
+    regresarMision(wolverine);
 })();
